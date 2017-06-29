49a5662b238d9222a3e59614551ed6b95edbc502
GuiltyAdonis <guiltyadonis@gmail.com>
2017-06-28 23:26:45 -0700

    Update jtgboards.js

--- jtgboards.js
+++ jtgboards.js
@@ -1,7 +1,7 @@
 var whitechapel = document.getElementById("cat-5115085");
     whitechapel.getElementsByClassName("c_info")[0].style.background = "url(http://i65.tinypic.com/ogx4qr.png)";
-    whitechapel.getElementsByClassName("c_info")[1].style.background = "url(http://i67.tinypic.com/30kvg9v.png)";
-    whitechapel.getElementsByClassName("c_info")[2].style.background = "url(http://i63.tinypic.com/vg29o7.png)";
-    whitechapel.getElementsByClassName("c_info")[3].style.background = "url(http://i67.tinypic.com/j923d0.png)";
-    whitechapel.getElementsByClassName("c_info")[4].style.background = "url(http://i67.tinypic.com/2s8pvr9.png)";
-    whitechapel.getElementsByClassName("c_info")[5].style.background = "url(http://i65.tinypic.com/2u9mp2d.png)";
+    whitechapel.getElementsByClassName("c_info")[1].style.background = "url(http://i67.tinypic.com/30kvg9v.png) 400px";
+    whitechapel.getElementsByClassName("c_info")[2].style.background = "url(http://i63.tinypic.com/vg29o7.png) 300px";
+    whitechapel.getElementsByClassName("c_info")[3].style.background = "url(http://i67.tinypic.com/j923d0.png) 400px";
+    whitechapel.getElementsByClassName("c_info")[4].style.background = "url(http://i67.tinypic.com/2s8pvr9.png) 400px";
+    whitechapel.getElementsByClassName("c_info")[5].style.background = "url(http://i65.tinypic.com/2u9mp2d.png) 165px -5px";
