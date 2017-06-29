23fbfcb670fc3fbbb7e07debd8f6e0a81991553c
GuiltyAdonis <guiltyadonis@gmail.com>
2017-06-28 23:08:24 -0700

    Create jtgboards.js

---
+++ jtgboards.js
@@ -0,0 +1,3 @@
+var whitechapel = document.getElementById("cat-5115085");
+    whitechapel.getElementsByClassName("c_info")[0].style.background = "url(http://b3.ifrm.com/40062/197/0/f7021092/whitechapelstation.jpg)";
+    
