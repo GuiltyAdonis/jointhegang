213dd515ab8a3dba9298024cdcf05e5b5374c2d7
GuiltyAdonis <guiltyadonis@gmail.com>
2017-06-28 23:21:12 -0700

    Update jtgboards.js

--- jtgboards.js
+++ jtgboards.js
@@ -1,3 +1,7 @@
 var whitechapel = document.getElementById("cat-5115085");
-    whitechapel.getElementsByClassName("c_info")[0].style.background = "url(http://b3.ifrm.com/40062/197/0/f7021092/whitechapelstation.jpg)";
-    
+    whitechapel.getElementsByClassName("c_info")[0].style.background = "url(http://i65.tinypic.com/ogx4qr.png)";
+    whitechapel.getElementsByClassName("c_info")[1].style.background = "url(http://i67.tinypic.com/30kvg9v.png)";
+    whitechapel.getElementsByClassName("c_info")[2].style.background = "url(http://i63.tinypic.com/vg29o7.png)";
+    whitechapel.getElementsByClassName("c_info")[3].style.background = "url(http://i67.tinypic.com/j923d0.png)";
+    whitechapel.getElementsByClassName("c_info")[4].style.background = "url(http://i67.tinypic.com/2s8pvr9.png)";
+    whitechapel.getElementsByClassName("c_info")[5].style.background = "url(http://i65.tinypic.com/2u9mp2d.png)";
