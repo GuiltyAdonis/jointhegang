e8432215d7281916137589a96c185c21cfe4ffe5
GuiltyAdonis <guiltyadonis@gmail.com>
2017-06-29 14:32:26 -0700

    Update jtgboards.js

--- jtgboards.js
+++ jtgboards.js
@@ -5,3 +5,11 @@ var whitechapel = document.getElementById("cat-5115085");
     whitechapel.getElementsByClassName("c_info")[3].style.background = "url(http://i67.tinypic.com/j923d0.png) 400px";
     whitechapel.getElementsByClassName("c_info")[4].style.background = "url(http://i67.tinypic.com/2s8pvr9.png) 400px";
     whitechapel.getElementsByClassName("c_info")[5].style.background = "url(http://i65.tinypic.com/2u9mp2d.png) 165px -5px";
+
+var london = document.getElementById("cat-5115086");
+    london.getElementsByClassName("c_info")[0].style.background = "url(http://i64.tinypic.com/33w45fs.jpg)";
+    london.getElementsByClassName("c_info")[1].style.background = "url(http://i67.tinypic.com/240ypsy.jpg)";
+    london.getElementsByClassName("c_info")[2].style.background = "url(http://i65.tinypic.com/2zstl6t.jpg)";
+    london.getElementsByClassName("c_info")[3].style.background = "url(http://i64.tinypic.com/2j337r7.jpg)";
+    london.getElementsByClassName("c_info")[4].style.background = "url(http://i64.tinypic.com/1z1s5r8.jpg)";
+    london.getElementsByClassName("c_info")[5].style.background = "url(http://i66.tinypic.com/x2p72f.jpg)";
