2feaa6bc37483fcb71d058fa1944c322570fb4d7
GuiltyAdonis <guiltyadonis@gmail.com>
2017-06-29 22:48:30 -0700

    Update jtgboards.js

--- jtgboards.js
+++ jtgboards.js
@@ -1,9 +1,10 @@
+/*
 var whitechapel = document.getElementById("cat-5115085");
     whitechapel.getElementsByClassName("c_info")[0].style.background = "url(http://i65.tinypic.com/ogx4qr.png)";
     whitechapel.getElementsByClassName("c_info")[1].style.background = "url(http://i67.tinypic.com/30kvg9v.png) 400px";
     whitechapel.getElementsByClassName("c_info")[2].style.background = "url(http://i63.tinypic.com/vg29o7.png) 300px";
     whitechapel.getElementsByClassName("c_info")[3].style.background = "url(http://i67.tinypic.com/j923d0.png) 400px";
-    whitechapel.getElementsByClassName("c_info")[4].style.background = "url(http://i67.tinypic.com/2s8pvr9.png) 400px";
+    whitechapel.getElementsByClassName("c_info")[4].style.background = "url(http://i67.tinypic.com/2s8pvr9.png) 400px"; //black market
     whitechapel.getElementsByClassName("c_info")[5].style.background = "url(http://i65.tinypic.com/2u9mp2d.png) 165px -5px";
 
 var london = document.getElementById("cat-5115086");
@@ -11,7 +12,7 @@ var london = document.getElementById("cat-5115086");
     london.getElementsByClassName("c_info")[1].style.background = "url(http://i67.tinypic.com/240ypsy.jpg)";
     london.getElementsByClassName("c_info")[2].style.background = "url(http://i65.tinypic.com/2zstl6t.jpg)";
     london.getElementsByClassName("c_info")[3].style.background = "url(http://i64.tinypic.com/2j337r7.jpg)";
-    london.getElementsByClassName("c_info")[4].style.background = "url(http://i63.tinypic.com/517b7q.png)";
+    london.getElementsByClassName("c_info")[4].style.background = "url(http://i63.tinypic.com/517b7q.png)"; //bank of london
     london.getElementsByClassName("c_info")[5].style.background = "url(http://i66.tinypic.com/x2p72f.jpg)";
 
 var westminster = document.getElementById("cat-5115087");
@@ -20,3 +21,4 @@ var westminster = document.getElementById("cat-5115087");
     westminster.getElementsByClassName("c_info")[2].style.background = "url(http://i65.tinypic.com/11ke7hw.jpg)";
     westminster.getElementsByClassName("c_info")[3].style.background = "url(http://i67.tinypic.com/291kche.jpg)";
     westminster.getElementsByClassName("c_info")[4].style.background = "url(http://i64.tinypic.com/wrcs55.jpg)";
+*/
